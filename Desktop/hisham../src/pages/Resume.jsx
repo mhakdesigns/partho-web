775988f42819c763c449bef9ac91652ca9d5247c
hishamak18 <hishamak2004@gmail.com>
2023-06-13 16:38:36 +0530

    CVs

--- Desktop/hisham../src/pages/Resume.jsx
+++ Desktop/hisham../src/pages/Resume.jsx
@@ -2,23 +2,35 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
-
-import Particle from "../components/Particle";
-import pdf from "../assets/Hisham_AK_CV.pdf";
-
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import Particle from "../components/Particle";
+import resumes from "../assets/Hisham_AK_CV.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/mhakdesigns/mhakcv/main/src/assets/Hisham_AK_CV.pdf`;
+const resumeLink = `https://raw.githubusercontent.com/mhakdesigns/mhakcv/main/Desktop/hisham../src/assets/Hisham_AK_CV.pdf`;
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+    setLoading(false);
+  };
+
+  const onPageError = (error) => {
+    setError(error);
+    setLoading(false);
+  };
 
   return (
     <div>
@@ -27,7 +39,7 @@ const Resume = () => {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={resumeLink}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -37,15 +49,20 @@ const Resume = () => {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document
+            file={resumes}
+            className="d-flex justify-content-center"
+            onLoadSuccess={onDocumentLoadSuccess}
+            onLoadError={onPageError}
+          >
+            <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={resumeLink}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
